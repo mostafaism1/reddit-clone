f60a2d10db1d95bfd4f4a172b736c3a05b48398e
mostafa <me.cssign@gmail.com>
2021-02-01 17:36:51 +0200

    fix type

--- src/main/java/com/example/redditclone/repository/VerificationTokenRepository.java
+++ src/main/java/com/example/redditclone/repository/VerificationTokenRepository.java
@@ -1,9 +1,11 @@
 package com.example.redditclone.repository;
 
+import com.example.redditclone.model.VerificationToken;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VerificationTokenRepository extends JpaRepository<VerificationTokenRepository, Long> {
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
 
 }
