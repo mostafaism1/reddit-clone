8e00756dd5a13dd0cb3817855fdea2aaad0fdee5
mostafa <me.cssign@gmail.com>
2021-02-01 16:48:50 +0200

    add missing @Repository annotation

--- src/main/java/com/example/redditclone/repository/CommentRepository.java
+++ src/main/java/com/example/redditclone/repository/CommentRepository.java
@@ -3,7 +3,9 @@ package com.example.redditclone.repository;
 import com.example.redditclone.model.Comment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
 }
--- src/main/java/com/example/redditclone/repository/UserRepository.java
+++ src/main/java/com/example/redditclone/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package com.example.redditclone.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.redditclone.model.User;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
