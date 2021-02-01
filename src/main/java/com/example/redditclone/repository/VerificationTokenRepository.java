932d4c744dcbde351567c2cff294a73b1036a3df
mostafa <me.cssign@gmail.com>
2021-02-01 16:37:00 +0200

    create initial repositories

---
+++ src/main/java/com/example/redditclone/repository/VerificationTokenRepository.java
@@ -0,0 +1,7 @@
+package com.example.redditclone.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VerificationTokenRepository extends JpaRepository<VerificationTokenRepository, Long> {
+
+}
