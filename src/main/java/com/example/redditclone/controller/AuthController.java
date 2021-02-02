49742df1c627ca0166a6b125a55d85f472ddfc4b
mostafa <me.cssign@gmail.com>
2021-02-02 05:57:28 +0200

    create an endpoint for user registration

---
+++ src/main/java/com/example/redditclone/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.example.redditclone.controller;
+
+import com.example.redditclone.dto.RegisterRequest;
+import com.example.redditclone.service.AuthService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping(path = "/api/auth")
+@AllArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
+
+        authService.signup(registerRequest);
+
+        return new ResponseEntity<>("User Registration Successful", HttpStatus.OK);
+    }
+
+}
