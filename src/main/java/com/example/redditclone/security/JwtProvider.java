785479373f29fd9bc964e57ab2b85ee9c4f03639
mostafa <me.cssign@gmail.com>
2021-02-02 17:04:03 +0200

    create a jwt service

---
+++ src/main/java/com/example/redditclone/security/JwtProvider.java
@@ -0,0 +1,36 @@
+package com.example.redditclone.security;
+
+import java.security.Key;
+
+import javax.annotation.PostConstruct;
+
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import org.springframework.security.core.userdetails.User;
+
+@Service
+public class JwtProvider {
+
+    private Key key;
+
+    @PostConstruct
+    public void init() {
+        key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+    }
+
+    public String generateToken(Authentication authentication) {
+        User principal = (User) authentication.getPrincipal();
+
+        return Jwts.builder()
+        .setSubject(principal.getUsername())
+        .signWith(key)
+        .compact();
+    }
+
+}
