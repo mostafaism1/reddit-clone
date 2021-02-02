01d77616cd5dbefb484d06365e1a677a061628fa
mostafa <me.cssign@gmail.com>
2021-02-02 22:17:43 +0200

    use java keystore for tokens

--- src/main/java/com/example/redditclone/security/JwtProvider.java
+++ src/main/java/com/example/redditclone/security/JwtProvider.java
@@ -1,36 +1,62 @@
 package com.example.redditclone.security;
 
-import java.security.Key;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
 
 import javax.annotation.PostConstruct;
 
+import com.example.redditclone.exception.RedditException;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
 
 import org.springframework.security.core.userdetails.User;
 
 @Service
 public class JwtProvider {
 
-    private Key key;
+    private KeyStore keyStore;
 
     @PostConstruct
-    public void init() {
-        key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+    public void init() {        
+        try {
+            keyStore = KeyStore.getInstance("JKS");
+            InputStream resourceAsStream = getClass().getResourceAsStream("/redditclone.jks");
+            keyStore.load(resourceAsStream, "mypassword".toCharArray());
+        } catch (KeyStoreException | CertificateException | NoSuchAlgorithmException | IOException e) {
+            throw new RedditException("Exception occurred while loading keystore", e);
+        }
     }
 
     public String generateToken(Authentication authentication) {
         User principal = (User) authentication.getPrincipal();
 
-        return Jwts.builder()
-        .setSubject(principal.getUsername())
-        .signWith(key)
-        .compact();
+        return Jwts.builder().setSubject(principal.getUsername()).signWith(getPrivateKey()).compact();
+    }
+
+    private PrivateKey getPrivateKey() {
+        try {
+            return (PrivateKey) keyStore.getKey("redditclone", "mypassword".toCharArray());
+        } catch (UnrecoverableKeyException | KeyStoreException | NoSuchAlgorithmException e) {
+            throw new RedditException("Exception occured while loading keystore");
+        }
+    }
+
+    private PublicKey getPublicKey() {
+        try {
+            return keyStore.getCertificate("redditclone").getPublicKey();
+        } catch (KeyStoreException e) {
+            throw new RedditException("Exception occured while retrieving public key from keystore");
+        }
     }
 
 }
