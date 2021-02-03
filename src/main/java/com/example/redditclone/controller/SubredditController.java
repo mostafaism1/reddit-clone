ffc0306f32e1d87080b8fbdbc9f579e4aa0b27c1
mostafa <me.cssign@gmail.com>
2021-02-03 14:27:50 +0200

    add an endpoint for getting all subreddits

--- src/main/java/com/example/redditclone/controller/SubredditController.java
+++ src/main/java/com/example/redditclone/controller/SubredditController.java
@@ -1,10 +1,13 @@
 package com.example.redditclone.controller;
 
+import java.util.List;
+
 import com.example.redditclone.dto.SubredditDto;
 import com.example.redditclone.service.SubredditService;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +26,11 @@ public class SubredditController {
     public ResponseEntity<SubredditDto> createSubreddit(@RequestBody SubredditDto subredditDto) {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(subredditService.save(subredditDto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SubredditDto>> getAllSubreddits() {
+        return ResponseEntity.status(HttpStatus.OK).body(subredditService.getAll());
     }
 
 }
