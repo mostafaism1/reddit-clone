0240fb3d4ada83c0442c94a43ce46a783c1291eb
mostafa <me.cssign@gmail.com>
2021-02-03 14:27:22 +0200

    create service to get all subreddits

--- src/main/java/com/example/redditclone/service/SubredditService.java
+++ src/main/java/com/example/redditclone/service/SubredditService.java
@@ -1,10 +1,14 @@
 package com.example.redditclone.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.example.redditclone.dto.SubredditDto;
 import com.example.redditclone.model.Subreddit;
 import com.example.redditclone.repository.SubredditRepository;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @AllArgsConstructor
 @Slf4j
+@Transactional
 public class SubredditService {
 
     private final SubredditRepository subredditRepository;
@@ -23,10 +28,17 @@ public class SubredditService {
     }
 
     private Subreddit mapSubredditDto(SubredditDto subredditDto) {
-        return Subreddit.builder()
-                            .name(subredditDto.getName())
-                            .description(subredditDto.getDescription())
-                        .build();
+        return Subreddit.builder().name(subredditDto.getName()).description(subredditDto.getDescription()).build();
     }
-    
+
+    public List<SubredditDto> getAll() {
+        return subredditRepository.findAll().stream().map(this::mapToDto).collect(Collectors.toList());
+
+    }
+
+    private SubredditDto mapToDto(Subreddit subreddit) {
+        return SubredditDto.builder().id(subreddit.getId()).name(subreddit.getName())
+                .description(subreddit.getDescription()).postCount(subreddit.getPosts().size()).build();
+    }
+
 }
