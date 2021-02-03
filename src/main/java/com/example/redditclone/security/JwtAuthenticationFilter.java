348cb051199d48d87b3d71c78b83163e7174e4cf
mostafa <me.cssign@gmail.com>
2021-02-03 15:00:19 +0200

    fix error passing wrong paramter

--- src/main/java/com/example/redditclone/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/redditclone/security/JwtAuthenticationFilter.java
@@ -34,15 +34,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             String username = jwtProvider.getUsernameFromJwt(jwt);
 
             UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-
-            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                    userDetails.getUsername(), null, userDetails.getAuthorities());
-
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails,
+                    null, userDetails.getAuthorities());
             authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
-
+        
         filterChain.doFilter(request, response);
     }
 
