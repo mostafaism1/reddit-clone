5473016b8264a204a827769ec8b0da30998f961a
mostafa <me.cssign@gmail.com>
2021-02-03 14:26:44 +0200

    add builder to SubredditDto

--- src/main/java/com/example/redditclone/dto/SubredditDto.java
+++ src/main/java/com/example/redditclone/dto/SubredditDto.java
@@ -1,12 +1,14 @@
 package com.example.redditclone.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class SubredditDto {
 
     private long id;
