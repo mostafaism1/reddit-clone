c2552dd4bac9c889cb45cc8f23660eff9335c70c
mostafa <me.cssign@gmail.com>
2021-02-03 13:51:15 +0200

    create subreddit dto

---
+++ src/main/java/com/example/redditclone/dto/SubredditDto.java
@@ -0,0 +1,19 @@
+package com.example.redditclone.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SubredditDto {
+
+    private long id;
+
+    private String name;
+
+    private String description;
+
+    private int postCount;
+}
