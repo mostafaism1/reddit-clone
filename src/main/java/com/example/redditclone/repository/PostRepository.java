542110c7d5becd6e6e8bb1a55d120739861f16ce
mostafa <me.cssign@gmail.com>
2021-02-06 11:50:13 +0200

    add methods to find Posts by a Subreddit, and by a User

--- src/main/java/com/example/redditclone/repository/PostRepository.java
+++ src/main/java/com/example/redditclone/repository/PostRepository.java
@@ -1,11 +1,20 @@
 package com.example.redditclone.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.example.redditclone.model.Post;
+import com.example.redditclone.model.Subreddit;
+import com.example.redditclone.model.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findBySubreddit(Subreddit subreddit);
+
+    List<Post> findByUser(User user);
 
 }
