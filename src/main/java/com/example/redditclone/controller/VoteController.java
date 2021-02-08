b13f1106fcda34a79213fd999a8fcca3ce14eaca
mostafa <me.cssign@gmail.com>
2021-02-08 16:05:46 +0200

    create a VoteController

---
+++ src/main/java/com/example/redditclone/controller/VoteController.java
@@ -0,0 +1,28 @@
+package com.example.redditclone.controller;
+
+import com.example.redditclone.dto.VoteDto;
+import com.example.redditclone.service.VoteService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/votes")
+@AllArgsConstructor
+public class VoteController {
+
+    private final VoteService voteService;
+
+    @PostMapping
+    public ResponseEntity<Void> createVote(@RequestBody VoteDto voteDto) {
+        voteService.save(voteDto);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
