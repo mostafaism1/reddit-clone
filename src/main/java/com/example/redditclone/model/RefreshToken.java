4060a528edb2d712cf46621965450184c2993aba
mostafa <me.cssign@gmail.com>
2021-02-10 20:26:58 +0200

    create a RefreshToken model

---
+++ src/main/java/com/example/redditclone/model/RefreshToken.java
@@ -0,0 +1,30 @@
+package com.example.redditclone.model;
+
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String token;
+
+    private Instant createdDate;
+
+}
