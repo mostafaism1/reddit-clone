02ed85273654daa0cf5621062c96df8601f5bbba
mostafa <me.cssign@gmail.com>
2021-02-10 20:32:15 +0200

    add a RefreshToken and expiry time to AuthenticationResponse

--- src/main/java/com/example/redditclone/dto/AuthenticationResponse.java
+++ src/main/java/com/example/redditclone/dto/AuthenticationResponse.java
@@ -1,16 +1,24 @@
 package com.example.redditclone.dto;
 
+import java.time.Instant;
+
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class AuthenticationResponse {
 
     private String authenticationToken;
 
+    private String refreshToken;
+
     private String username;
+
+    private Instant expiresAt;
 
 }
