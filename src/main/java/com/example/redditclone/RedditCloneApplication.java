7f22c4305a2f1405e62e59f021fa3dea5dfc292d
mostafa <me.cssign@gmail.com>
2021-02-11 12:39:43 +0200

    import swagger configuration on application startup

--- src/main/java/com/example/redditclone/RedditCloneApplication.java
+++ src/main/java/com/example/redditclone/RedditCloneApplication.java
@@ -1,11 +1,15 @@
 package com.example.redditclone;
 
+import com.example.redditclone.config.SwaggerConfig;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfig.class)
 public class RedditCloneApplication {
 
 	public static void main(String[] args) {
