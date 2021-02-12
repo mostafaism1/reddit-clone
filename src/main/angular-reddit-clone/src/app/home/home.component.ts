1a798076b23f5cc09fbf4ff2e533741f1637e815
mostafa <me.cssign@gmail.com>
2021-02-12 20:12:53 +0200

    refactor all post display from home-component to post-tile component

--- src/main/angular-reddit-clone/src/app/home/home.component.ts
+++ src/main/angular-reddit-clone/src/app/home/home.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { faArrowUp, faArrowDown, faComments }from '@fortawesome/free-solid-svg-icons'
-import { PostModel } from '../shared/post-model';
-import { PostService } from '../shared/post.service';
 
 @Component({
   selector: 'app-home',
@@ -11,30 +8,10 @@ import { PostService } from '../shared/post.service';
 
 export class HomeComponent implements OnInit {
 
-  posts: PostModel[];
-
-  faArrowUp = faArrowUp;
-  faArrowDown = faArrowDown;
-  faComments = faComments;
-
-
-  constructor(private postService: PostService) {    
+  constructor() {
   }
 
   ngOnInit(): void {
-    this.postService.getAllPosts().subscribe(posts => this.posts = posts);
   }
 
-  upvotePost() {
-
-  }
-
-  downvotePost() {
-
-  }
-  
-  goToPost(postId: number) {
-
-  }
-  
 }
--- src/main/angular-reddit-clone/src/app/shared/post-tile/post-tile.component.ts
+++ src/main/angular-reddit-clone/src/app/shared/post-tile/post-tile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { faArrowDown, faArrowUp, faComments } from '@fortawesome/free-solid-svg-icons';
+import { PostModel } from '../post-model';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-post-tile',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostTileComponent implements OnInit {
 
-  constructor() { }
+  posts: PostModel[];
+
+  faArrowUp = faArrowUp;
+  faArrowDown = faArrowDown;
+  faComments = faComments;
+
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
+    this.postService.getAllPosts().subscribe(posts => this.posts = posts);
+  }
+
+  upvotePost() {
+
+  }
+
+  downvotePost() {
+
+  }
+
+  goToPost(postId: number) {
+
   }
 
 }
