9f54143b71714ada60e7cea6b38ec17891cb6b87
mostafa <me.cssign@gmail.com>
2021-02-12 19:50:39 +0200

    remove commented out old Post model definition

--- src/main/angular-reddit-clone/src/app/shared/post-model.ts
+++ src/main/angular-reddit-clone/src/app/shared/post-model.ts
@@ -1,15 +1,3 @@
-// export class PostModel {
-//     id: number;
-//     postName: string;
-//     url: string;
-//     description: string;
-//     voteCount: number;
-//     userName: string;
-//     subredditName: string;
-//     commentCount: number;
-//     duration: string;
-// }
-
 export class PostModel {
     id: number
     name: string
