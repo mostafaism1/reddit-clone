022160f8df1a22bfef27aad525851313fb2b9f28
mostafa <me.cssign@gmail.com>
2021-02-12 19:47:02 +0200

    create a post model

---
+++ src/main/angular-reddit-clone/src/app/shared/post-model.ts
@@ -0,0 +1,23 @@
+// export class PostModel {
+//     id: number;
+//     postName: string;
+//     url: string;
+//     description: string;
+//     voteCount: number;
+//     userName: string;
+//     subredditName: string;
+//     commentCount: number;
+//     duration: string;
+// }
+
+export class PostModel {
+    id: number
+    name: string
+    description: string
+    url: string
+    duration: string
+    voteCount: number
+    commentCount: number
+    subredditName: string
+    userName: string
+}
