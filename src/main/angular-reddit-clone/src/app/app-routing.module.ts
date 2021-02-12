efa48d5a9add664c85089d8ce463421169e84e2a
mostafa <me.cssign@gmail.com>
2021-02-12 22:15:35 +0200

    add route for create-subreddit component

--- src/main/angular-reddit-clone/src/app/app-routing.module.ts
+++ src/main/angular-reddit-clone/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { HomeComponent } from './home/home.component';
+import { CreateSubredditComponent } from './subreddit/create-subreddit/create-subreddit.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'sign-up', component: SignupComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'create-subreddit', component: CreateSubredditComponent }
 ];
 
 @NgModule({
