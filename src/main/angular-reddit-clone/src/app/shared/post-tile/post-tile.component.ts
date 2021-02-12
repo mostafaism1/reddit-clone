d4114da627b70f7b29a02288d157eda37114a83a
mostafa <me.cssign@gmail.com>
2021-02-13 00:04:43 +0200

    add navigation from post-tile to view-post

--- src/main/angular-reddit-clone/src/app/shared/post-tile/post-tile.component.ts
+++ src/main/angular-reddit-clone/src/app/shared/post-tile/post-tile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { faComments } from '@fortawesome/free-solid-svg-icons';
 import { PostModel } from '../post-model';
 import { PostService } from '../post.service';
@@ -13,14 +14,14 @@ export class PostTileComponent implements OnInit {
   posts: PostModel[];
   faComments = faComments;
 
-  constructor(private postService: PostService) { }
+  constructor(private postService: PostService, private router: Router) { }
 
   ngOnInit(): void {
     this.postService.getAllPosts().subscribe(posts => this.posts = posts);
   }
 
   goToPost(postId: number) {
-
+    this.router.navigateByUrl('view-post/' + postId);
   }
 
 }
