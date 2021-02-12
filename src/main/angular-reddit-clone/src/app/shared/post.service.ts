86ea7e1c5cc17848ae0cfc4f6c375a6acfbed0c7
mostafa <me.cssign@gmail.com>
2021-02-12 19:48:29 +0200

    add getAllPosts service

--- src/main/angular-reddit-clone/src/app/shared/post.service.ts
+++ src/main/angular-reddit-clone/src/app/shared/post.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PostModel } from './post-model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,4 +10,7 @@ export class PostService {
 
   constructor(private http: HttpClient) { }
 
+  getAllPosts(): Observable<Array<PostModel>> {
+    return this.http.get<Array<PostModel>>('http://localhost:8080/api/posts/');
+  }
 }
