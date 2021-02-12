16e81456cf7557104d83013e58438095fde28f1e
mostafa <me.cssign@gmail.com>
2021-02-12 22:58:49 +0200

    load and display all subreddits

--- src/main/angular-reddit-clone/src/app/subreddit/list-subreddits/list-subreddits.component.ts
+++ src/main/angular-reddit-clone/src/app/subreddit/list-subreddits/list-subreddits.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { throwError } from 'rxjs';
+import { SubredditModel } from '../subreddit-model';
+import { SubredditService } from '../subreddit.service';
 
 @Component({
   selector: 'app-list-subreddits',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-subreddits.component.css']
 })
 export class ListSubredditsComponent implements OnInit {
+  subreddits: SubredditModel[];
 
-  constructor() { }
+  constructor(private subredditService: SubredditService) { }
 
   ngOnInit(): void {
+    this.subredditService.getAllSubreddits().subscribe(
+      subreddits => {
+        this.subreddits = subreddits;
+      },
+      error => {
+        throwError(error);
+      });
   }
 
 }
