de0ac024831eb37d924e7f0692c94a46816f2409
mostafa <me.cssign@gmail.com>
2021-02-12 23:40:01 +0200

    add a form and event handlers for create-post component

--- src/main/angular-reddit-clone/src/app/post/create-post/create-post.component.ts
+++ src/main/angular-reddit-clone/src/app/post/create-post/create-post.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { throwError } from 'rxjs';
+import { PostModel } from 'src/app/shared/post-model';
+import { PostService } from 'src/app/shared/post.service';
+import { SubredditModel } from 'src/app/subreddit/subreddit-model';
+import { SubredditService } from 'src/app/subreddit/subreddit.service';
 
 @Component({
   selector: 'app-create-post',
@@ -7,9 +14,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatePostComponent implements OnInit {
 
-  constructor() { }
+  createPostForm: FormGroup;
+  postModel: PostModel;
+  subreddits: SubredditModel[];
+
+  constructor(private router: Router, private postService: PostService, private subredditService: SubredditService) {
+    this.createPostForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      subredditName: new FormControl('', Validators.required),
+      url: new FormControl('', Validators.required),
+      description: new FormControl('', Validators.required)
+    });
+
+    this.postModel = new PostModel();
+  }
 
   ngOnInit(): void {
+    this.subredditService.getAllSubreddits().subscribe(
+      subreddits => {
+        this.subreddits = subreddits;
+      },
+      error => {
+        throwError(error);
+      });
+  }
+
+  createPost() {
+    this.postModel.name = this.createPostForm.get('name')?.value;
+    this.postModel.subredditName = this.createPostForm.get('subredditName')?.value;
+    this.postModel.url = this.createPostForm.get('url')?.value;
+    this.postModel.description = this.createPostForm.get('description')?.value;
+
+    this.postService.createPost(this.postModel).subscribe(
+      (data) => {
+        this.router.navigateByUrl('/');
+      },
+      (error) => {
+        throwError(error);
+      }
+    );
+  }
+
+  discardPost() {
+    this.router.navigateByUrl('/');
   }
 
 }
