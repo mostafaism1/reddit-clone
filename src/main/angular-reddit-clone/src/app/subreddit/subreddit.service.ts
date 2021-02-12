b0745596cc95be2fe9dfdb13fb3a39ff1538240f
mostafa <me.cssign@gmail.com>
2021-02-12 21:35:14 +0200

    add getAllSubreddits service

--- src/main/angular-reddit-clone/src/app/subreddit/subreddit.service.ts
+++ src/main/angular-reddit-clone/src/app/subreddit/subreddit.service.ts
@@ -1,10 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SubredditModel } from './subreddit-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubredditService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
+  getAllSubreddits(): Observable<SubredditModel[]> {
+    return this.http.get<SubredditModel[]>('http://localhost:8080/api/subreddits');
+  }
 }
