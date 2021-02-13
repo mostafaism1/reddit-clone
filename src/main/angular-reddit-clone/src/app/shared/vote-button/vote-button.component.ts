d3cac7c9e594f738145d02c6d2f3188799943458
mostafa <me.cssign@gmail.com>
2021-02-13 21:38:38 +0200

    add handlers for voting

--- src/main/angular-reddit-clone/src/app/shared/vote-button/vote-button.component.ts
+++ src/main/angular-reddit-clone/src/app/shared/vote-button/vote-button.component.ts
@@ -1,6 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { faArrowDown, faArrowUp } from '@fortawesome/free-solid-svg-icons';
+import { ToastrService } from 'ngx-toastr';
+import { throwError } from 'rxjs';
+import { AuthService } from 'src/app/auth/shared/auth.service';
 import { PostModel } from '../post-model';
+import { PostService } from '../post.service';
+import { VoteType } from '../vote-type';
+import { VoteModel } from '../vote.model';
+import { VoteService } from '../vote.service';
 
 @Component({
   selector: 'app-vote-button',
@@ -10,21 +17,44 @@ import { PostModel } from '../post-model';
 export class VoteButtonComponent implements OnInit {
 
   @Input() post: PostModel;
-
+  voteModel: VoteModel;  
   faArrowUp = faArrowUp;
-  faArrowDown = faArrowDown;
+  faArrowDown = faArrowDown;  
 
-  constructor() { }
+  constructor(private voteService: VoteService, private postService: PostService, private toastrService: ToastrService) {
+    this.voteModel = {
+      voteType: undefined,
+      postId: undefined
+    }
+   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {    
   }
 
   upvotePost() {
-
+    this.voteModel.voteType = VoteType.UPVOTE;
+    this.vote();
   }
 
   downvotePost() {
+    this.voteModel.voteType = VoteType.DOWNVOTE;
+    this.vote();
+  }  
 
+  vote() {
+    this.voteModel.postId = this.post.id;
+    this.voteService.vote(this.voteModel).subscribe(() => {
+      this.updateVoteDetails();
+    }, error => {
+      this.toastrService.error(error.error.message);
+      throwError(error);
+    });
   }
 
+  updateVoteDetails() {
+    this.postService.getPost(this.post.id).subscribe(
+      (post) => {
+        this.post = post;
+      })
+  }
 }
