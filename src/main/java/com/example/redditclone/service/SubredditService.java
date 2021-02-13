26d92e6267a7cf9e3e783fc91617c6ca83e7e2f8
mostafa <me.cssign@gmail.com>
2021-02-13 12:51:42 +0200

    remove mapStruct dependencies

--- src/main/java/com/example/redditclone/service/SubredditService.java
+++ src/main/java/com/example/redditclone/service/SubredditService.java
@@ -1,11 +1,11 @@
 package com.example.redditclone.service;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import com.example.redditclone.dto.SubredditDto;
 import com.example.redditclone.exception.RedditException;
-import com.example.redditclone.mapper.subredditMapper;
 import com.example.redditclone.model.Subreddit;
 import com.example.redditclone.repository.SubredditRepository;
 
@@ -20,25 +20,33 @@ import lombok.AllArgsConstructor;
 public class SubredditService {
 
     private final SubredditRepository subredditRepository;
-    private final subredditMapper subredditMapper;
+    private final AuthService authService;
 
     public SubredditDto save(SubredditDto subredditDto) {
-        Subreddit subreddit = subredditRepository.save(subredditMapper.mapDtoToSubreddit(subredditDto));
+        Subreddit subreddit = subredditRepository.save(mapDtoToSubreddit(subredditDto));
         subredditDto.setId(subreddit.getId());
         return subredditDto;
-    }    
-
+    }
 
     public List<SubredditDto> getAll() {
-        return subredditRepository.findAll().stream().map(subredditMapper::mapSubredditToDto).collect(Collectors.toList());
+        return subredditRepository.findAll().stream().map(this::mapSubredditToDto).collect(Collectors.toList());
 
     }
 
-
-	public SubredditDto getSubreddit(long id) {
+    public SubredditDto getSubreddit(long id) {
         Subreddit subreddit = subredditRepository.findById(id)
-            .orElseThrow(() -> new RedditException("No subreddit found with id = " + id));
-        return subredditMapper.mapSubredditToDto(subreddit);
-	}
+                .orElseThrow(() -> new RedditException("No subreddit found with id = " + id));
+        return mapSubredditToDto(subreddit);
+    }
+
+    private Subreddit mapDtoToSubreddit(SubredditDto subredditDto) {
+        return Subreddit.builder().name(subredditDto.getName()).description(subredditDto.getDescription())
+                .user(authService.getCurrentUser()).createdAt(Instant.now()).build();
+    }
+
+    private SubredditDto mapSubredditToDto(Subreddit subreddit) {
+        return SubredditDto.builder().id(subreddit.getId()).name(subreddit.getName())
+                .description(subreddit.getDescription()).postCount(subreddit.getPosts().size()).build();
+    }
 
 }
