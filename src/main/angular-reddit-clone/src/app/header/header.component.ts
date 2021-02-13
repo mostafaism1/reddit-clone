f4b920d26beb82ffdefe1366b54f82fb1daa1ba3
mostafa <me.cssign@gmail.com>
2021-02-13 15:35:54 +0200

    add a username display with logout and user-profile navigations to the header

--- src/main/angular-reddit-clone/src/app/header/header.component.ts
+++ src/main/angular-reddit-clone/src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from '../auth/shared/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -6,20 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  faUser = faUser;
   isLoggedIn: boolean = false;
   username:String = "";
 
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+    this.isLoggedIn = this.authService.isLoggedIn();
+    this.username = this.authService.getUserName();
   }
 
   goToUserProfile(): void {
-
+    this,this.router.navigateByUrl('/user-profile/' + this.username);
   }
 
   logout(): void {
+    this.authService.logout();
+    
+    this.router.navigateByUrl('').then(() => {
+      window.location.reload();
+    })
 
   }
 
