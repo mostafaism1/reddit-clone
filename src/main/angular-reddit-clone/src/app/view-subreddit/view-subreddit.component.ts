d64bb0c03707c7b5b54f6db49a0195efe2719405
mostafa <me.cssign@gmail.com>
2021-02-13 23:57:06 +0200

    get and display subreddit information and posts

--- src/main/angular-reddit-clone/src/app/view-subreddit/view-subreddit.component.ts
+++ src/main/angular-reddit-clone/src/app/view-subreddit/view-subreddit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PostModel } from '../shared/post-model';
+import { PostService } from '../shared/post.service';
+import { SubredditModel } from '../subreddit/subreddit-model';
+import { SubredditService } from '../subreddit/subreddit.service';
 
 @Component({
   selector: 'app-view-subreddit',
@@ -6,10 +11,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-subreddit.component.css']
 })
 export class ViewSubredditComponent implements OnInit {
+  
+  subredditName: string;
+  subreddit: SubredditModel;
+  posts: PostModel[];
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private postService: PostService, private subredditService: SubredditService) { 
+    this.subreddit = new SubredditModel();
+  }
 
   ngOnInit(): void {
+    this.subredditName = this.activatedRoute.snapshot.params.subredditName;
+    this.subredditService.getSubredditByName(this.subredditName).subscribe(subreddit => {
+      this.subreddit = subreddit;
+      this.postService.getPostsBySubreddit(this.subreddit.id).subscribe(posts => this.posts = posts);    
+    });    
   }
 
 }
