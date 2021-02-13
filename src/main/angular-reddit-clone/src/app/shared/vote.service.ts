e930838d6eeab18c2011507392c1d55d40867c53
mostafa <me.cssign@gmail.com>
2021-02-13 17:02:25 +0200

    add vote service

--- src/main/angular-reddit-clone/src/app/shared/vote.service.ts
+++ src/main/angular-reddit-clone/src/app/shared/vote.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { VoteModel } from './vote.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VoteService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  vote(voteModel: VoteModel): Observable<any> {
+    return this.http.post<any>('http://localhost:8080/api/votes/', voteModel);
+  }
 }
